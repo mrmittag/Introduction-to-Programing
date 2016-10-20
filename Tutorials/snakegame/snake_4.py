095c36d19d5a3df632b51a8223bd8b72252cd812
Seth Mittag <mittags@MBP-MittagS-0815.local>
2016-10-19 22:05:44 -0400

    adding snake game

--- Tutorials/snakegame/snake_4.py
+++ Tutorials/snakegame/snake_4.py
@@ -4,10 +4,11 @@ import time
 screen = curses.initscr()
 screen.border()
 screen.nodelay(1)
-head=[1,1]
 screen.keypad(1)
+
 direction =0 
 game_over =False  
+head=[1,1]
 
 def processInput(direction):
     """This function processes any user input"""
@@ -37,16 +38,35 @@ def update(direction):
     screen.refresh()
     time.sleep(0.1)
     
+def collison(direction):
+    """This function is used to detect a collision"""
+    if direction == 3 and screen.inch(head[0]-1,head[1]) !=ord(' '):
+        return True
+    elif direction == 2 and screen.inch(head[0]+1,head[1]) !=ord(' '):
+        return True
+    elif direction == 1 and screen.inch(head[0],head[1]-1) !=ord(' '):
+        return True
+    elif direction == 0 and screen.inch(head[0],head[1]+1) !=ord(' '):
+        return True    
+    else:
+        return False
     
 
 def display():
     """This function draws all of the stuff in the game"""
     screen.addch(head[0],head[1],'x')
-      
+
+# def start_screen():
+#     pass
+#
+# start_screen()
+#
 while game_over == False:
     direction = processInput(direction)
     update(direction)
     display()
- 
+    game_over = collison(direction)
+    
+
     
 

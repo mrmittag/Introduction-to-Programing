35916642ecb54429f72042fc778e75b55c2e8bbb
mrmittag <mistermittag@gmail.com>
2016-11-21 08:33:33 -0500

    clean up

--- Tutorials/snakegame/gameclass/board.py
+++ Tutorials/snakegame/gameclass/board.py
@@ -1,5 +1,3 @@
-
-
 class Board():
     """Class for drawing game board"""
     def __init__(self,screen):
@@ -11,10 +9,5 @@ class Board():
     def display(self):
         self.screen.addstr(3,3,"This is the board")
        
-if __name__ =="__main__":
-    try:
-        print("board  worked")
-    except:
-        print("board didnt work")
-    
+
     
